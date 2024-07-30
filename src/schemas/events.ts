e58674df3730bd3db8e210c3cf7053502657e75f
Tevin Obai <tevinobai0409@gmail.com>
2024-07-30 10:21:36 +0300

    modified end point for event creation

--- src/schemas/events.ts
+++ src/schemas/events.ts
@@ -8,10 +8,11 @@ export const EventSchema = z.object({
     .default(() => new Date().toISOString()),
   description: z.string(),
   id: z.string().cuid().optional(),
-  eventCreatedById: z.string().cuid(),
+  eventCreatedById: z.string().cuid().optional(),
   location: z.object({
     longitude: z.number(),
     latitude: z.number(),
+    name: z.string().optional(),
   }),
   mapData: z.object({
     longitude: z.number(),
@@ -19,7 +20,12 @@ export const EventSchema = z.object({
     longitudeDelta: z.number(),
     latitudeDelta: z.number(),
   }),
-  photos: z.array(z.string().url()),
+  photos: z.array(
+    z.object({
+      url: z.string().url(),
+      name: z.string(),
+    })
+  ),
   price: z.number().nonnegative(),
   title: z.string(),
   updatedAt: z
